b56740735634bb52206a52cb4ffc13bf3bc33aa5
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-03-09 16:59:02 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,17 +2,33 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 
+
+
 //For env File
 dotenv.config();
+const mysql = require("mysql")
 const cors = require("cors")
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
-app.get('/api/home', (req, res) => {
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
 
-    res.json({message: "Hello Simon"});
-});
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySQL connected!")
+    }
+})
+
+const bcrypt = require("bcryptjs")
+
+
 
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
