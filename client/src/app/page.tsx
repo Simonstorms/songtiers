dea7196a423f0b8695547d30cddf6dbc7f2009ba
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-03-22 12:24:58 +0100

    client first steps

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,22 +2,24 @@
 "use client";
 
 import { useEffect } from "react";
+import {UploadForm} from "@/components/upload";
 
 export default function Home() {
-    useEffect(() => {
-        fetch("http://localhost:8000/api/home")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     fetch("http://localhost:8000/api/home")
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching data:', error);
+    //         });
+    // }, []);
 
     return (
         <main>
             <h1>Hello world</h1>
+            <UploadForm />
         </main>
     );
 }
