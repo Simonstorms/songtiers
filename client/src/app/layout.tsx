fae0f5d3507dbf79682dd87938c093c0dee3471d
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-04-17 20:49:37 +0200

    Update layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,40 +1,36 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
-import {ReactNode, } from "react";
-import { cookies } from 'next/headers'
-// import {FetchUser} from "@/components/authentification/FetchUser";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ReactNode } from "react";
+import { cookies } from "next/headers";
+import { FetchUser } from "@/components/authentification/FetchUser";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Linkal - Analyse your LinkedIn chats",
-  description: "by Simon Gneuss",
+    title: "Linkal - Analyse your LinkedIn chats",
+    description: "by Simon Gneuss",
 };
 
 type RootLayoutProps = { children: ReactNode };
 
-    //ciehe slack
 export default function RootLayout({ children }: RootLayoutProps) {
-    const session = cookies().get('cookie_jwt')?.value;
-    console.log(session);
-    //const user = await userLoader(session);
-  return (
-      <>
-        <html lang="en" suppressHydrationWarning>
-        <head />
-        <body className={`${inter.className}`}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-          {/*<FetchUser user={user}>{children}</FetchUser>*/}
-        </ThemeProvider>
-        </body>
-        </html>
-      </>
-  )
+    return (
+        <>
+            <html lang="en" suppressHydrationWarning>
+                <head />
+                <body>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                        <FetchUser>{children}</FetchUser>
+                    </ThemeProvider>
+                </body>
+            </html>
+        </>
+    );
 }
