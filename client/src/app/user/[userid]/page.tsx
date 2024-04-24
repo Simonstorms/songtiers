144a7f932bf5bbc3bca8e278789ec2c2430a469f
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-04-24 23:13:16 +0200

    erste fortschritte im image machen

--- client/src/app/user/[userid]/page.tsx
+++ client/src/app/user/[userid]/page.tsx
@@ -1,26 +1,47 @@
 "use client";
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 
 import dynamic from "next/dynamic";
 import Navbar from "@/components/Navbar";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
-import ScreenshotButton from "@/components/Screenshot";
 
 const SongField = dynamic(() => import("@/components/SongField"), {
     ssr: false,
 });
-const Page: FC = () => {
-    const captureRef = useRef(null);
+import domtoimage from "dom-to-image";
+import { Button } from "@/components/ui/button";
 
+const Page: FC = () => {
+    const takeScreenshot = () => {
+        const element = document.body;
+        if (element) {
+            domtoimage
+                .toPng(element)
+                .then((dataUrl) => {
+                    const link = document.createElement("a");
+                    link.download = "screenshot.png";
+                    link.href = dataUrl;
+                    link.click();
+                })
+                .catch((error) => {
+                    console.error("oops, something went wrong!", error);
+                });
+        } else {
+            console.error("Body element not found");
+        }
+    };
     return (
         <BackgroundGradientAnimation>
             <Navbar headline={"Select your Top 5"} />
-            <div className="flex z-50 absolute flex-col items-center  gap-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div
+                id="user-page"
+                className="flex z-50 absolute flex-col items-center  gap-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            >
                 {" "}
                 {[1, 2, 3, 4, 5].map((position) => (
                     <SongField key={position} position={position} />
                 ))}
-                <ScreenshotButton captureRef={captureRef} />
+                <Button onClick={takeScreenshot}>Take Screenshot</Button>
             </div>
         </BackgroundGradientAnimation>
     );
