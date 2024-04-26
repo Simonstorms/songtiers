610d62384e814c3306c153c5e70447fb737b79e2
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-04-26 20:28:57 +0200

    add: about me

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ArrowDown } from "lucide-react";
 
 export default function Home() {
     return (
@@ -30,9 +32,34 @@ export default function Home() {
                         </Link>
                     </motion.div>
 
-                    <h2 className="text-4xl font-bold leading-[70px]">
-                        Thats me
-                    </h2>
+                    <div className="flex flex-col items-center justify-center">
+                        <h2 className="text-4xl font-bold leading-[70px]">
+                            Yup, that's me.
+                        </h2>
+                        <h3 className="sm:text-xl   py-5">
+                            ou're probably wondering how I ended up in this
+                            situation
+                        </h3>
+                        <p className={"leading-[70px]"}>Check it out</p>
+                        <ArrowDown size={50} />
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            <Link href={"https://www.simongneuss.com/"}>
+                                <Image
+                                    src={"/mac.png"}
+                                    alt={"Apple Emoji of me"}
+                                    height={150}
+                                    width={150}
+                                />
+                            </Link>
+                        </motion.div>
+                    </div>
                 </div>
             </main>
         </>
